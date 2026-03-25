ab0347d63dc4d789a49ec8e00277a4cdeeeccc97
Pankaj8821 <pankaj8821886202@gmail.com>
2026-03-25 16:04:07 +0530

    working

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const PORT = 5000;
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from Backend 🚀" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
